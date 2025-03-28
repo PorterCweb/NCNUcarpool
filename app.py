c80bdc73f93ed7ebd60d4c756cd7d65e5b4082fe
PorterCweb <402poter0222@gmail.com>
2025-03-28 21:44:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -210,7 +210,19 @@ def get_key(dict, value):
     return [k for k, v in dict.items() if v == value]
 #   獲取 GoogleSheet 的司機、揪團試算表
 #       token.json的資料如放在公開伺服器上執行之類的，會遭停用，需到google cloud重新建立金鑰，否則會跳出JWP錯誤
-gc = gspread.service_account(os.getenv('GOOGLE_CREDENTIALS'))   
+import tempfile
+import os
+import json
+import gspread
+from dotenv import load_dotenv
+load_dotenv()
+# 從環境變數讀取 JSON 字串，否則env只能傳入string，且env檔須調整為單行。
+credentials_str = os.getenv("GOOGLE_CREDENTIALS")
+if credentials_str:
+    credentials_dict = json.loads(credentials_str)
+    print(credentials_dict)
+# 使用修正後的字典創建 gspread 客戶端
+gc = gspread.service_account_from_dict(credentials_dict)
 carpool = gc.open_by_url('https://docs.google.com/spreadsheets/d/1q8HKO2NBz1O8UBE7ag9Kq-eNAc114TKzkXyOq32vfSA/edit?gid=1437248658#gid=1437248658')
 driver_sheet = carpool.get_worksheet(0)
 passenger_sheet = carpool.get_worksheet(1)
@@ -1641,3 +1653,5 @@ schedule.every(7).seconds.do(check_project)
 scheduler_thread = threading.Thread(target=run_scheduler)
 scheduler_thread.daemon = True 
 scheduler_thread.start()
+
+
