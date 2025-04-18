8d6ac3703d4ae6ba43b8ec1768e20b5c0cbd4572
PorterCweb <402poter0222@gmail.com>
2025-04-18 15:11:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -358,9 +358,9 @@ def run_scheduler():
     while a:
         schedule.run_pending()
         time.sleep(0.1)  
-schedule.every(30).minutes.do(check_project)
-schedule.every(30).seconds.do(get_driver_sheet_case)
-schedule.every(30).seconds.do(get_passenger_sheet_case)
+schedule.every(10).minutes.do(check_project)
+schedule.every(20).seconds.do(get_driver_sheet_case)
+schedule.every(20).seconds.do(get_passenger_sheet_case)
 scheduler_thread_case = threading.Thread(target=run_scheduler)
 scheduler_thread_case.daemon = True  # 主程式結束此也結束
 scheduler_thread_case.start()
