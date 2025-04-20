0ffca77b283ed552ac2ee0338cf063585132b25d
PorterCweb <402poter0222@gmail.com>
2025-04-20 22:33:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -472,20 +472,6 @@ def handle_message(event):
                                     },
                                     {
                                         "type": "text",
-                                        "text": f"手機號碼：{driver_sheet[i][13]}",
-                                        "color": "#000000",
-                                        "size": "xs",
-                                        "decoration": "underline"
-                                    },
-                                    {
-                                        "type": "text",
-                                        "text": f"LineID：{driver_sheet[i][10]}",
-                                        "color": "#000000",
-                                        "size": "xs",
-                                        "decoration": "underline"
-                                    },
-                                    {
-                                        "type": "text",
                                         "text": f"共乘人數上限：{driver_sheet[i][5]}",
                                         "color": "#000000",
                                         "size": "xs"
@@ -506,7 +492,7 @@ def handle_message(event):
                                     "paddingAll": "20px",
                                     "backgroundColor": "#0367D3",
                                     "spacing": "md",
-                                    "height": "300px",
+                                    "height": "265px",
                                     "paddingTop": "22px"
                                 },
                                 "body": {
@@ -754,20 +740,6 @@ def handle_message(event):
                                     },
                                     {
                                         "type": "text",
-                                        "text": f"手機號碼：{passenger_sheet[i][12]}",
-                                        "color": "#000000",
-                                        "size": "xs",
-                                        "decoration": "underline"
-                                    },
-                                    {
-                                        "type": "text",
-                                        "text": f"LineID：{passenger_sheet[i][10]}",
-                                        "color": "#000000",
-                                        "size": "xs",
-                                        "decoration": "underline"
-                                    },
-                                    {
-                                        "type": "text",
                                         "text": f"共乘人數上限：{passenger_sheet[i][5]}",
                                         "color": "#000000",
                                         "size": "xs"
@@ -782,7 +754,7 @@ def handle_message(event):
                                     "paddingAll": "20px",
                                     "backgroundColor": "#0367D3",
                                     "spacing": "md",
-                                    "height": "275px",
+                                    "height": "240px",
                                     "paddingTop": "22px"
                                 },
                                 "body": {
@@ -978,7 +950,7 @@ def handle_message(event):
                     driver_case_launchdatetime = parse_custom_time(driver_sheet[i][0])
                     driver_case_launchdate = driver_case_launchdatetime.strftime("%Y-%m-%d")
                     if driver_case_date >= now_date or driver_case_launchdate == now_date:
-                        reservation = f'共乘編號：{driver_sheet[i][17]}\n發車地點：{driver_sheet[i][2]}\n目的地：{driver_sheet[i][4]}\n出發時間：\n{driver_sheet[i][3]}\n總時程：{time_hrmi(int(driver_sheet[i][6]))}\n發起人：{driver_sheet[i][9]}\n手機號碼：{driver_sheet[i][13]}\nLineID：{driver_sheet[i][10]}\n共乘人數上限：{driver_sheet[i][5]}\n價格：{driver_sheet[i][11]}\n交通工具：{driver_sheet[i][12]}\n行車規範：\n{driver_sheet[i][7]}\n簡介：{driver_sheet[i][8]}\n'
+                        reservation = f'共乘編號：{driver_sheet[i][17]}\n發車地點：{driver_sheet[i][2]}\n目的地：{driver_sheet[i][4]}\n出發時間：\n{driver_sheet[i][3]}\n總時程：{time_hrmi(int(driver_sheet[i][6]))}\n發起人：{driver_sheet[i][9]}\n共乘人數上限：{driver_sheet[i][5]}\n價格：{driver_sheet[i][11]}\n交通工具：{driver_sheet[i][12]}\n行車規範：\n{driver_sheet[i][7]}\n簡介：{driver_sheet[i][8]}\n'
                         driver_text = driver_text+reservation+'--------------------------------\n'
                     else:
                         pass
@@ -1034,7 +1006,7 @@ def handle_postbak(event):
                     line_bot_api = MessagingApi(api_client)
                     if driver_case_date>now_date or driver_case_launchdate == now_date:
                         confirm_template = ConfirmTemplate(
-                            text = f'共乘編號：{driver_sheet[i][17]}\n發車地點：{driver_sheet[i][2]}\n目的地：{driver_sheet[i][4]}\n出發時間：\n{driver_sheet[i][3]}\n總時程：{time_hrmi(int(driver_sheet[i][6]))}\n發起人：{driver_sheet[i][9]}\n手機號碼：{driver_sheet[i][13]}\nLineID：{driver_sheet[i][10]}\n共乘人數上限：{driver_sheet[i][5]}\n價格：{driver_sheet[i][11]}\n交通工具：{driver_sheet[i][12]}\n行車規範：\n{driver_sheet[i][7]}\n簡介：{driver_sheet[i][8]}\n',
+                            text = f'共乘編號：{driver_sheet[i][17]}\n發車地點：{driver_sheet[i][2]}\n目的地：{driver_sheet[i][4]}\n出發時間：\n{driver_sheet[i][3]}\n總時程：{time_hrmi(int(driver_sheet[i][6]))}\n發起人：{driver_sheet[i][9]}\n共乘人數上限：{driver_sheet[i][5]}\n價格：{driver_sheet[i][11]}\n交通工具：{driver_sheet[i][12]}\n行車規範：\n{driver_sheet[i][7]}\n簡介：{driver_sheet[i][8]}\n',
                             actions=[ #只能放兩個Action
                                 PostbackAction(label='確定搭乘', text='確定!',data=f'driver_Sure{i}'),
                                 MessageAction(label='再考慮', text='再考慮')
@@ -1129,7 +1101,7 @@ def handle_postbak(event):
                     line_bot_api = MessagingApi(api_client)
                     if passenger_case_date > now_date or passenger_case_launchdate == now_date:
                         confirm_template = ConfirmTemplate(
-                            text = f'共乘編號：{passenger_sheet[i][16]}\n發車地點：{passenger_sheet[i][2]}\n目的地：{passenger_sheet[i][4]}\n出發時間：\n{passenger_sheet[i][3]}\n總時程：{time_hrmi(int(passenger_sheet[i][6]))}\n發起人：{passenger_sheet[i][9]}\n手機號碼：{passenger_sheet[i][12]}\nLineID：{passenger_sheet[i][10]}\n共乘人數上限：{passenger_sheet[i][5]}\n交通工具：{passenger_sheet[i][11]}\n行車規範：\n{passenger_sheet[i][7]}\n簡介：{passenger_sheet[i][8]}\n',
+                            text = f'共乘編號：{passenger_sheet[i][16]}\n發車地點：{passenger_sheet[i][2]}\n目的地：{passenger_sheet[i][4]}\n出發時間：\n{passenger_sheet[i][3]}\n總時程：{time_hrmi(int(passenger_sheet[i][6]))}\n發起人：{passenger_sheet[i][9]}\n共乘人數上限：{passenger_sheet[i][5]}\n交通工具：{passenger_sheet[i][11]}\n行車規範：\n{passenger_sheet[i][7]}\n簡介：{passenger_sheet[i][8]}\n',
                             actions=[ #一定只能放兩個Action
                                 PostbackAction(label='確定搭乘', text='確定!', data=f'passenger_Sure{i}'),
                                 MessageAction(label='再考慮', text='再考慮')   
